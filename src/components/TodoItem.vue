f2bd896ae82ff5909ddae47eda0fb5ff5700d784
maskwes <stalin1995.gerasimov@yandex.ru>
2020-06-08 15:00:48 +0300

    0.7

--- src/components/TodoItem.vue
+++ src/components/TodoItem.vue
@@ -3,7 +3,7 @@
     <span v-bind:class="{done: todo.completed}">
         <input type="checkbox" name="" id="" v-on:change="todo.completed=!todo.completed">
         <strong>{{index+1}}</strong>
-        {{todo.title}}
+        {{todo.title | uppercase}}
     </span>
     <button class="rm" v-on:click="$emit('remove-todo',todo.id)">&times;</button>
 </li>
@@ -19,6 +19,12 @@ export default {
             required:true
         },
         index:Number
+    },
+    filters:{
+        uppercase(value){
+            console.log(value)
+            return value.toUpperCase()
+        }
     }
 
 }
--- src/views/Todos.vue
+++ src/views/Todos.vue
@@ -4,26 +4,67 @@
     <router-link to="/">Home</router-link>
     <hr>
     <AddTodo @add-todo="addTodo"></AddTodo>
+    <select v-model="filter">
+      <option value="all">All</option>
+      <option value="completed">completed</option>
+      <option value="not-completed">not-completed</option>
+    </select>
     <hr>
-    <Todolist v-bind:todos="todos" @remove-todo="removeTodo"></Todolist>
+    <Loader v-if="loading"></Loader>
+    <Todolist 
+    v-else-if="filteredTodos.length"
+        v-bind:todos="filteredTodos" 
+        @remove-todo="removeTodo">
+    </Todolist>
+    <p v-else>No todos!</p>
   </div>
 </template>
 
 <script>
 import Todolist from'@/components/Todo-list'
 import AddTodo from'@/components/AddTodo'
+import Loader from'@/components/Loader'
 
 export default {
   name: 'App',
   data(){
     return{
-      todos:[]
+      todos:[],
+      loading:true,
+      filter:'all'
     }
   },
   mounted() {
   fetch('https://jsonplaceholder.typicode.com/todos?_limit=3')
     .then(response => response.json())
-    .then(json => this.todos=json)
+    .then(json => {this.todos=json
+    setTimeout(()=>{ 
+      this.todos=json
+      this.loading=false
+      },1000)
+    })
+    
+  },
+  // watch:{
+  //   filter(value){
+  //     console.log(value)
+  //   }
+  // },
+  computed:{
+    filteredTodos(){
+      if(this.filter==='all')
+      {
+        return this.todos
+      }
+      if(this.filter==='completed')
+      {
+        return this.todos.filter(t=>t.completed)
+      }
+      if(this.filter==='not-completed')
+      {
+        return this.todos.filter(t=>!t.completed)
+      }
+    }
   },
   methods:{
     removeTodo(id){
@@ -35,7 +76,8 @@ export default {
   },
   components: {
     Todolist,
-    AddTodo
+    AddTodo,
+    Loader
   }
 }
 </script>
