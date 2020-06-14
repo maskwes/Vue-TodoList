5bf837212fbd999ed858b2be5d37587ade88c763
maskwes <stalin1995.gerasimov@yandex.ru>
2020-06-14 16:02:11 +0300

    0.9.7.5

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,14 +1,29 @@
 <template>
   <div>
+    <v-navigation-drawer absolute temporary v-model="drawer">
+       <v-list>
+           <v-list-tile v-for="(item,i) in menuItems":key="i">
+               <v-list-tile-action>
+                   <v-icon v-html="item.icon"></v-icon>
+               </v-list-tile-action>
+               <v-list-item-title></v-list-item-title>
+               <v-list-tile-content>
+                 <v-list-tile-title v-text="item.title"></v-list-tile-title>
+               </v-list-tile-content>
+           </v-list-tile>
+       </v-list>
+    </v-navigation-drawer>
+
     <v-app-bar
       color="deep-purple accent-4"
       dense
       dark
     >
+    <v-app-bar-nav-icon></v-app-bar-nav-icon>
       <v-toolbar-title>Todo</v-toolbar-title>
       <v-spacer></v-spacer>
         <v-toolbar-items>
-            <v-btn text v-for="(item,i) in menuItems"  :to="item.route">
+            <v-btn text v-for="(item,i) in menuItems":key="i"  :to="item.route">
                <v-icon left v-html="item.icon"></v-icon>
                 {{item.title}}
             </v-btn>
@@ -18,6 +33,11 @@
 </template>
 <script>
 export default {
+    data(){
+        return{
+            drawer:false
+        }
+    },
     computed:{
         menuItems(){
             return[
@@ -59,7 +79,7 @@ export default {
 </script>
 
 <style scoped>
-a:not(.md-button):hover{
-    text-decoration: underline;
-}
+    a:not(.md-button):hover{
+        text-decoration: underline;
+    }
 </style>
